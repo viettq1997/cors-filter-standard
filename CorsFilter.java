7188b2698c606c7d3cf81f89fd451325fbe8607e
Tran Quoc Viet <vietpro23w@gmail.com>
2021-12-26 23:50:51 +0700

    first commit

---
+++ CorsFilter.java
@@ -0,0 +1,44 @@
+package com.viettqit.userservice.filter;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/***
+ * @author Tran Quoc Viet
+ * @version 1.0
+ * @since 12/26/2021
+ */
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)  //the value Ordered.HIGHEST_PRECEDENCE can be used for overriding the highest priority among components.
+public class CorsFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
+        response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
+        HttpServletRequest request = (HttpServletRequest) req;
+        if ("OPTIONS".equals(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
